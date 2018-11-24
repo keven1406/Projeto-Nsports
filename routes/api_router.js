30e3f86388cbffd354b9421fac81426f8e99caa3
Elias Keven <eliaskeven@gmail.com>
2018-11-24 11:24:01 -0300

    adicionando tela admin

--- routes/api_router.js
+++ routes/api_router.js
@@ -6,4 +6,8 @@ api.get("/", (req, res) => {
 	res.render("index.ejs")
 })
 
+api.get("/criar-produtos", (req, res) => {
+	res.render("./admin/criar-produtos/inserir-produtos.ejs")
+})
+
 module.exports = api
