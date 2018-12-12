a6a72dd88adf0035b8ba45096c7ce280a9c091fd
Elias Keven <eliaskeven@gmail.com>
2018-12-12 17:13:15 -0300

    adicionando post /login

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
 const apiRouter = require("./routes/api_router")
-
+const mongoose = require("mongoose")
 const app = express()
-	
+
+mongoose.connect("mongodb://localhost:27017/test")
+
 //views
 app.set("view engine", "ejs")
 app.set("views", path.resolve(__dirname, "views"))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set("port", process.env.PORT || 3000)
 //static server
 const pathStatic = path.resolve(__dirname, "public")
 app.use(express.static(pathStatic))
@@ -16,6 +21,6 @@ app.use(express.static(pathStatic))
 //Router for API V1.0
 app.use("/v1", apiRouter)
 
-app.use((req, res) => res.status(404).send("Page not found!"))
+app.use((req, res) => res.status(404).send("Error 404. Page not found!"))
 
-app.listen(3000, () => console.log("Server started on port 3000"))
+app.listen(app.get("port"), () => console.log("Server started on port " + app.get("port")))
--- routes/api_router.js
+++ routes/api_router.js
@@ -1,5 +1,7 @@
 const express = require("express")
 
+const User = require("/models/user.js")
+
 const api = express.Router()
 
 api.get("/", (req, res) => {
@@ -9,5 +11,30 @@ api.get("/", (req, res) => {
 api.get("/criar-produtos", (req, res) => {
 	res.render("./admin/criar-produtos/inserir-produtos.ejs")
 })
+
+api.get("/login", (req, res) => {
+	res.render("./login.ejs")
+})
+
+api.get("/user", (req, res) => {
+	res.send("Entrou =)")
+})
+
+api.post("/login", function (req, res, next) {
+	const username = req.body.username
+	const password = req.body.password
+	User.findOne({ username: username }), function (err, user) {
+		if (err) { next(err) } 
+		if (user) {
+			req.flash("error", "Usuario já existe")
+			res.redirect("/login")
+		}
+	}
+
+})
+
+api.get("/logout")
+
+api.post("/logout")
 
 module.exports = api
